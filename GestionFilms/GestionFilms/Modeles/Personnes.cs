427a6e17e3d5825f04f4f247569f8d7de49fe0cd
Kouame Alain N'Gouandi <kalain20@yahoo.fr>
2025-05-06 23:10:26 -0400

    Ajout des accesseurs à la classe Personnes

--- GestionFilms/GestionFilms/Modeles/Personnes.cs
+++ GestionFilms/GestionFilms/Modeles/Personnes.cs
@@ -9,19 +9,55 @@ namespace GestionFilms.Modeles
     public class Personnes
     {
         private int _id;
-        private string _nom;
-        private string _prenom;
+        private string? _nom;
+        private string? _prenom;
         
-        private string _telephone;
+        private string? _telephone;
 
 
         public int Id { get { return _id; } set { _id = value; } }
-        public string Nom { get { return _nom; } set { _nom = value; } }
+        public string Nom 
+        { 
+            get 
+            { 
+                if (_nom == null)
+                    _nom = string.Empty;
+                return _nom; 
+            } 
+            set 
+            { 
+                _nom = value; 
+            } 
+        }
 
-        public string Prenom { get { return _prenom; } set { _prenom = value; } }
+        public string Prenom 
+        { 
+            get
+            { 
+                if (_prenom == null)    
+                    _prenom = string.Empty;
+               
+              return _prenom; 
+            } 
+            set 
+            { _prenom = value; 
+            } 
+        }
 
         
 
-        public string Telephone { get { return _telephone; } set { _telephone = value; } }
+        public string Telephone 
+        { 
+            get 
+            {
+                if (_telephone == null)
+                    return "() --- ----";
+                return _telephone; 
+            } 
+            set
+            { 
+                _telephone = value; 
+            }
+        }
     }
 }
