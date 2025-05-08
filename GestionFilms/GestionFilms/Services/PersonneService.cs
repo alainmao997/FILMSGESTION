bcbe88558c7afb4f897ef523dc9a7f0b599bac8c
Kouame Alain N'Gouandi <kalain20@yahoo.fr>
2025-05-07 21:57:18 -0400

    Ajustement des classes Personne - Client  et création de service pour less oprations dsur la classe personne

---
+++ GestionFilms/GestionFilms/Services/PersonneService.cs
@@ -0,0 +1,48 @@
+﻿
+using GestionFilms.IServices;
+using GestionFilms.Modeles;
+
+namespace GestionFilms.Services
+{
+    public class PersonneService : IPersonnesService
+    {
+        private List<Personne> _personnes;
+
+        public PersonneService()
+        {
+            _personnes = new List<Personne>();
+        }
+        public void AjouterPersonne(Personne personne)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModifierPersonne(Personne personne)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Personne> ObtenirListePersonnes()
+        {
+            return this._personnes;
+        }
+
+        public Personne ObtenirPersonne(int id)
+        {
+            if (id < 0)
+                throw new ArgumentException("L'identifiant doit être un nombre positif.", nameof(id));
+
+            var personne = _personnes.FirstOrDefault(p => p.Id == id);
+
+            if(personne == null)
+                throw new KeyNotFoundException($"Aucune personne trouvée avec l'ID {id}.");
+
+            return personne;
+        }
+
+        public void SupprimerPersonne(Personne personne)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
